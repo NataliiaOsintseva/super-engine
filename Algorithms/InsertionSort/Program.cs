4239ff44fe8bfdd01b109f5d97c0b4e53692fc5b
Nataliia Osintseva <nataly.osya@gmail.com>
2016-08-14 18:26:31 +0300

    Added Shell and Insertion sort

---
+++ Algorithms/InsertionSort/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        public static int[] InsertionSort(int[] intArray)
+        {
+
+            int temp;
+            int j;
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                temp = intArray[i];
+                j = i - 1;
+
+                while (j >= 0 && intArray[j] > temp)
+                {
+                    intArray[j + 1] = intArray[j];
+                    j--;
+                }
+
+                intArray[j + 1] = temp;
+            }
+
+            return intArray;
+        }
+
+        public static TimeSpan Benchmark(int elNum)
+        {
+            int[] intArray = new int[elNum];
+
+            Random rand = new Random();
+
+            for (int x = 0; x < intArray.Length; x++)
+            {
+                intArray[x] = rand.Next(1, elNum);
+            }
+
+            // Uncomment this section, if you want to print array
+            Console.WriteLine("-------UnSorted Array Input-------");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+
+            intArray = InsertionSort(intArray);
+
+            timer.Stop();
+
+            // Uncomment this section, if you want to print array
+            Console.WriteLine("-------Array After Isertion Sort-------");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            return timer.Elapsed;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Benchmark(5));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/ShellSort/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    class Program
+    {
+        public static int[] ShellSort(int[] intArray)
+        {
+            int gap = intArray.Length / 2;
+            while (gap > 0)
+            {
+                for(int i = 0; i < intArray.Length - gap; i++)
+                {
+                    int j = i + gap;
+                    int temp = intArray[j];
+
+                    while (j >= gap && temp > intArray[j - gap])
+                    {
+                        intArray[j] = intArray[j - gap];
+                        j -= gap;
+                    }
+                    intArray[j] = temp;
+                }
+                if (gap == 2)
+                {
+                    gap = 1;
+                }
+                else
+                {
+                    gap = (int)(gap / 2.2);
+                }
+            }
+
+            return intArray;
+        }
+
+        public static TimeSpan Benchmark(int elNum)
+        {
+            int[] intArray = new int[elNum];
+
+            Random rand = new Random();
+
+            for (int x = 0; x < intArray.Length; x++)
+            {
+                intArray[x] = rand.Next(1, elNum);
+            }
+
+            // Uncomment this section, if you want to print array
+            Console.WriteLine("-------UnSorted Array Input-------");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+
+            intArray = ShellSort(intArray);
+
+            timer.Stop();
+
+            // Uncomment this section, if you want to print array
+            Console.WriteLine("-------Array After Shell Sort-------");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            return timer.Elapsed;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Benchmark(5));
+
+            Console.ReadKey();
+        }
+
+    }
+}
